2d9f76e6ef789c9227e09073dfaa4ca7ec46db4e
maxim <filbertmilkin@gmail.com>
2020-11-28 16:21:08 +0200

    Try aws credentials

--- main.tf
+++ main.tf
@@ -26,3 +26,13 @@ terraform {
 provider "aws" {
   region = "eu-central-1"
 }
+
+
+resource "aws_s3_bucket" "example" {
+  bucket = "example"
+}
+
+resource "aws_s3_access_point" "example" {
+  bucket = aws_s3_bucket.example.id
+  name   = "example"
+}
