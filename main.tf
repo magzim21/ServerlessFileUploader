45075d385d47364211aee3e83ff40c45d4634312
maxim <filbertmilkin@gmail.com>
2020-12-08 09:33:02 +0200

    Create public s3 with unique name

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+variable "aws_access_key_id" {
+  type = string
+}
+variable "aws_secret_access_key" {
+  type = string
+}
+
+
+
 # The configuration for the `remote` backend.
 terraform {
   backend "remote" {
@@ -25,14 +34,46 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "eu-central-1"
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
 }
+<<<<<<< Updated upstream
+=======
 
 
-resource "aws_s3_bucket" "example" {
-  bucket = "example"
-}
+# resource "aws_s3_bucket" "example" {
+#   bucket = "example"
+# }
 
-resource "aws_s3_access_point" "example" {
-  bucket = aws_s3_bucket.example.id
-  name   = "example"
+# resource "aws_s3_access_point" "example" {
+#   bucket = aws_s3_bucket.example.id
+#   name   = "example"
+# }
+
+resource "aws_s3_bucket" "static-website" {
+  bucket = "s3-website-test.hashicorp.com"
+  acl    = "public-read"
+  tags = {
+    Name        = "FileUploaderStatic-{{SED-GITHUB_REPOSITORY}}-{{SED-GITHUB_REF}}"
+    #Name        = "{{!SED-GITHUB_REPOSITORY}}-{{!SED-GITHUB_REF}}"
+    # Environment = "Dev"
+  }
+  # policy = file("policy.json")
+
+#   website {
+#     index_document = "index.html"
+#     error_document = "error.html"
+
+#     routing_rules = <<EOF
+# [{
+#     "Condition": {
+#         "KeyPrefixEquals": "docs/"
+#     },
+#     "Redirect": {
+#         "ReplaceKeyPrefixWith": "documents/"
+#     }
+# }]
+# EOF
+#   }
 }
+>>>>>>> Stashed changes
